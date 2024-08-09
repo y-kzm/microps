a136cd71f708bbd088e6c852f1cd55225428bcea
y-kzm <kzm-yokoo@hiroshima-u.ac.jp>
2024-08-10 03:08:16 +0900

    Using tap device

--- test/test6.c
+++ test/test6.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <signal.h>
 #include <unistd.h>
-#include <sys/types.h>
 
 #include "util.h"
 #include "net.h"
 #include "ip6.h"
-#include "icmp6.h"
 
-#include "driver/null.h"
 #include "driver/loopback.h"
 #include "driver/ether_tap.h"
 
@@ -24,15 +20,12 @@ on_signal(int s)
     terminate = 1;
 }
 
-int
-main(int argc, char *argv[])
+static int
+setup(void)
 {
     struct net_device *dev;
     struct ip6_iface *iface;
 
-    /*
-     * Setup protocol stack
-     */
     signal(SIGINT, on_signal);
     if (net_init() == -1) {
         errorf("net_init() failure");
@@ -43,8 +36,21 @@ main(int argc, char *argv[])
         errorf("loopback_init() failure");
         return -1;
     }
-
     iface = ip6_iface_alloc(LOOPBACK_IPV6_ADDR, LOOPBACK_IPV6_PREFIX_LEN);
+    if (!iface) {
+        errorf("ip6_iface_alloc() failure");
+        return -1;
+    }
+    if (ip6_iface_register(dev, iface) == -1) {
+        errorf("ip6_iface_register() failure");
+        return -1;
+    }
+    dev = ether_tap_init(ETHER_TAP_NAME, ETHER_TAP_HW_ADDR);
+    if (!dev) {
+        errorf("ether_tap_init() failure");
+        return -1;
+    }
+    iface = ip6_iface_alloc(ETHER_TAP_IPV6_ADDR, ETHER_TAP_IPV6_PREFIX_LEN);
     if (!iface) {
         errorf("ip6_iface_alloc() failure");
         return -1;
@@ -57,23 +63,25 @@ main(int argc, char *argv[])
         errorf("net_run() failure");
         return -1;
     }
-    /*
-     * Test Code
-     */
-    ip6_addr_t src, dst;
-    size_t offset = IPV6_HDR_SIZE + 4;
-    ip6_addr_pton(LOOPBACK_IPV6_ADDR, &src);
-    dst = src;
+    return 0;
+}
+
+static void
+cleanup(void)
+{
+    net_shutdown();
+}
+
+int
+main(int argc, char *argv[])
+{
+    if (setup() == -1) {
+        errorf("setup() failure");
+        return -1;
+    }
     while (!terminate) {
-        if (icmp6_output(ICMPV6_TYPE_ECHO_REQUEST, 0, test_data + offset, sizeof(test_data) - offset, src, dst) == -1) {
-            errorf("icmp6_output() failure");
-            break;
-        }
         sleep(1);
     }
-    /*
-     * Cleanup protocol stack
-     */
-    net_shutdown();
+    cleanup();
     return 0;
 }
--- test/test6.h
+++ test/test6.h
@@ -3,8 +3,15 @@
 
 #include <stdint.h>
 
+#define ETHER_TAP_NAME "tap0"
+/* Scope of EUI-48 Documentation Values. see https://tools.ietf.org/html/rfc7042 */
+#define ETHER_TAP_HW_ADDR "00:00:5e:00:53:01"
+/* Scope of Documentation Address Blocks (TEST-NET-1). see https://tools.ietf.org/html/rfc5731 */
 #define LOOPBACK_IPV6_ADDR "::1"
 #define LOOPBACK_IPV6_PREFIX_LEN 128
+#define ETHER_TAP_IPV6_ADDR "2001:db8::2"
+#define ETHER_TAP_IPV6_PREFIX_LEN 64
+
 
 const uint8_t test_data[] = {
     0x60, 0x0d, 0x03, 0x00,
