9bf263d11e43e067053e996f8174a4ce3a0d3023
y-kzm <kzm-yokoo@hiroshima-u.ac.jp>
2024-08-10 00:56:32 +0900

    Add icmpv6 input

--- icmp6.c
+++ icmp6.c
@@ -8,18 +8,141 @@
 
 #define ICMP6_BUFSIZ IPV6_PAYLOAD_SIZE_MAX
 
+struct pseudo6_hdr {
+    ip6_addr_t src;
+    ip6_addr_t dst;
+    uint32_t len;
+    uint8_t zero[3];
+    uint8_t next;
+};
+
+struct icmp6_hdr {
+	uint8_t	type;
+	uint8_t	code;
+	uint16_t sum;
+    uint8_t data[];
+};
+
+struct icmp6_echo {
+    uint8_t type;
+    uint8_t code;
+    uint16_t sum;
+    uint16_t id;
+    uint16_t seq;
+    uint8_t data[];
+};
+
+static char *
+icmp6_type_ntoa(uint8_t type) {
+    switch (type) {
+    case ICMPV6_TYPE_DEST_UNREACH:
+        return "DestinationUnreachable";
+    case ICMPV6_TYPE_TOO_BIG:
+        return "Too Big"; 
+    case ICMPV6_TYPE_TIME_EXCEEDED:
+        return "Time Exceeded";
+    case ICMPV6_TYPE_PARAM_PROBLEM:
+        return "Parameter Problem";
+    case ICMPV6_TYPE_ECHO_REQUEST:
+        return "Echo Request";
+    case ICMPV6_TYPE_ECHO_REPLY:
+        return "Echo Reply";
+    case ICMPV6_TYPE_ROUTER_SOL:
+        return "Router Solicitation";
+    case ICMPV6_TYPE_ROUTER_ADV:
+        return "Router Advertisement";
+    case ICMPV6_TYPE_NEIGHBOR_SOL:
+        return "Neighbor Solicitation";
+    case ICMPV6_TYPE_NEIGHBOR_ADV:
+        return "Neighbor Advertisement";
+    case ICMPV6_TYPE_REDIRECT:
+        return "Redirect";
+    }
+    return "Unknown";
+}
+
+void 
+icmp6_dump(const uint8_t *data, size_t len)
+{
+    struct icmp6_hdr *hdr;
+    struct icmp6_echo *echo;
+
+    flockfile(stderr);
+    hdr = (struct icmp6_hdr *)data;
+    fprintf(stderr, "       type: %s (%u)\n", icmp6_type_ntoa(hdr->type), hdr->type);
+    fprintf(stderr, "       code: %u\n", hdr->code);
+    fprintf(stderr, "        sum: 0x%04x\n", ntoh16(hdr->sum));
+    switch (hdr->type) {
+    case ICMPV6_TYPE_DEST_UNREACH:
+    case ICMPV6_TYPE_TOO_BIG:
+    case ICMPV6_TYPE_TIME_EXCEEDED:
+    case ICMPV6_TYPE_PARAM_PROBLEM:
+        break;
+    case ICMPV6_TYPE_ECHO_REQUEST:
+    case ICMPV6_TYPE_ECHO_REPLY:
+        echo = (struct icmp6_echo *)hdr;
+        fprintf(stderr, "         id: %u\n", ntoh16(echo->id));
+        fprintf(stderr, "        seq: %u\n", ntoh16(echo->seq));
+        break;
+    case ICMPV6_TYPE_ROUTER_SOL:
+    case ICMPV6_TYPE_ROUTER_ADV:
+    case ICMPV6_TYPE_NEIGHBOR_SOL:
+    case ICMPV6_TYPE_NEIGHBOR_ADV:
+    case ICMPV6_TYPE_REDIRECT:
+        break;
+    default:
+        break;
+    }
+#ifdef HEXDUMP
+    hexdump(stderr, data, len);
+#endif
+    funlockfile(stderr);
+}
+
 static void
 icmp6_input(const uint8_t *data, size_t len, ip6_addr_t src, ip6_addr_t dst, struct ip6_iface *iface)
 {
+    struct pseudo6_hdr pseudo;
+    uint16_t psum = 0;
+    struct icmp6_hdr *hdr;
     char addr1[IPV6_ADDR_STR_LEN];
     char addr2[IPV6_ADDR_STR_LEN];
     char addr3[IPV6_ADDR_STR_LEN];
 
-    debugf("%s => %s, len=%zu, iface=%s",
+    if (len < sizeof(*hdr)) {
+        errorf("too short");
+        return;
+    }
+    hdr = (struct icmp6_hdr *)data;
+    IPV6_ADDR_COPY(&pseudo.src, &src, sizeof(pseudo.src));
+    IPV6_ADDR_COPY(&pseudo.dst, &dst, sizeof(pseudo.dst));
+    pseudo.len = hton32(len);
+    pseudo.zero[0] = pseudo.zero[1] = pseudo.zero[2] = 0;
+    pseudo.next = IPV6_PROTOCOL_ICMPV6;
+    psum = ~cksum16((uint16_t *)&pseudo, sizeof(pseudo), 0);
+    if (cksum16((uint16_t *)data, len, psum) != 0) {
+        errorf("checksum error, sum=0x%04x, verify=0x%04x", ntoh16(hdr->sum), ntoh16(cksum16((uint16_t *)data, len, -hdr->sum + psum)));
+        return;
+    }
+    debugf("%s => %s, type=%s(%u), len=%zu, iface=%s",
         ip6_addr_ntop(src, addr1, sizeof(addr1)),
-        ip6_addr_ntop(dst, addr2, sizeof(addr2)), len,
+        ip6_addr_ntop(dst, addr2, sizeof(addr2)), 
+        icmp6_type_ntoa(hdr->type), hdr->type, len,
         ip6_addr_ntop(iface->addr, addr3, sizeof(addr3)));
-    debugdump(data, len);
+    icmp6_dump(data, len);
+    switch (hdr->type) {
+    case ICMPV6_TYPE_ECHO_REQUEST:
+        break;
+    case ICMPV6_TYPE_ROUTER_SOL:
+    case ICMPV6_TYPE_ROUTER_ADV:
+    case ICMPV6_TYPE_NEIGHBOR_SOL:
+    case ICMPV6_TYPE_NEIGHBOR_ADV:
+    case ICMPV6_TYPE_REDIRECT:
+        break;
+    default:
+        /* ignore */
+        break;
+    }
 }
 
 int
--- icmp6.h
+++ icmp6.h
@@ -8,6 +8,21 @@
 
 #define ICMPV6_HDR_SIZE 8
 
+/* for error messages */
+#define ICMPV6_TYPE_DEST_UNREACH    1
+#define ICMPV6_TYPE_TOO_BIG         2 
+#define ICMPV6_TYPE_TIME_EXCEEDED   3
+#define ICMPV6_TYPE_PARAM_PROBLEM   4
+
+/* for informational messages */
+#define ICMPV6_TYPE_ECHO_REQUEST    128
+#define ICMPV6_TYPE_ECHO_REPLY      129
+#define ICMPV6_TYPE_ROUTER_SOL      133
+#define ICMPV6_TYPE_ROUTER_ADV      134
+#define ICMPV6_TYPE_NEIGHBOR_SOL    135
+#define ICMPV6_TYPE_NEIGHBOR_ADV    136
+#define ICMPV6_TYPE_REDIRECT        137
+
 extern int
 icmp6_init(void);
 
